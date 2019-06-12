7bc175fd074791a5d0037e27c6ceb868fa681bb5
achkh17 <achkh17@freeuni.edu.ge>
2019-06-12 14:56:00 +0400

    created userInfo table

--- passwords.sql
+++ passwords.sql
@@ -4,12 +4,16 @@ DROP TABLE IF EXISTS passwords;
  -- remove table if it already exists and start from scratch
 
 CREATE TABLE IF NOT EXISTS passwords (
-    user_id VARCHAR(20) not null,
+    user_id integer not null,
     pass VARCHAR(15) not null
 );
 
 
 INSERT INTO passwords VALUES
-    ("anamaria", "bla"),
+   /* ("anamaria", "bla"),
     ("mariami", "blu"),
-    ("nino", "blura");
+    ("nino", "blura"); */
+    
+	(1, "bla"),
+    (2, "blu"),
+    (3, "blura");
--- userInfo.sql
+++ userInfo.sql
@@ -1,15 +1,24 @@
 USE finalProject;
 
-DROP TABLE IF EXISTS passwords;
+DROP TABLE IF EXISTS userInfo;
  -- remove table if it already exists and start from scratch
 
 CREATE TABLE IF NOT EXISTS userInfo (
-    user_id int not null,
-    pass VARCHAR(15) not null
+    user_id int not null auto_increment,
+    user_name varchar(15) unique not null,
+    first_name varchar(15),
+    last_name varchar(15),
+    email varchar(50) not null,
+    date_of_birth date,
+    gender varchar(10) not null,
+    img varchar(50),
+    rank varchar(20) default 'begginer',
+    primary key(user_id)
 );
 
 
-INSERT INTO passwords VALUES
-    ("anamaria", "bla"),
-    ("mariami", "blu"),
-    ("nino", "blura");
+
+
+INSERT INTO userInfo (user_name, first_name, last_name, email, date_of_birth, gender, img, rank) VALUES
+    ('achkh17', 'anamaria', 'chkhaidze', 'achkh17@freeuni.edu.ge', sysdate(), 'female', 'bla.jpg', 'beginner'),
+    ('nchan17', 'nino', 'chanturia', 'nchan17@freeuni.edu.ge', sysdate(), 'female', 'blura.jpg', 'beginner');
