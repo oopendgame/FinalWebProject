f7f868f248e92ae12e905c8b74c2b60dacaf8ae0
achkh17 <achkh17@freeuni.edu.ge>
2019-06-12 13:07:37 +0400

    connected to database

---
+++ src/WebPackage/DBConnection.java
@@ -0,0 +1,30 @@
+package WebPackage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBConnection {
+	
+	private static Connection con;
+	
+	public DBConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection( "jdbc:mysql://" + DBInfo.MYSQL_DATABASE_SERVER,
+												DBInfo.MYSQL_USERNAME, DBInfo.MYSQL_PASSWORD);
+			Statement stmt = con.createStatement();
+	        stmt.executeQuery("USE " + DBInfo.MYSQL_DATABASE_NAME);
+	        ResultSet rs = stmt.executeQuery("SELECT * FROM webTable");
+
+		} catch(Exception e) {
+			System.out.println("can't connect to database");
+		}
+	}	
+	
+	public static Connection getConnection() {
+		return con;
+	}
+}
+
---
+++ src/WebPackage/DBInfo.java
@@ -0,0 +1,8 @@
+package WebPackage;
+
+public class DBInfo {
+	public static final String MYSQL_USERNAME = "root";
+	public static final String MYSQL_PASSWORD = "W)jmz~5%fLW?T86C";
+	public static final String MYSQL_DATABASE_SERVER = "localhost";
+	public static final String MYSQL_DATABASE_NAME = "mysql";
+}
