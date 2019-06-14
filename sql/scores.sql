048aac3e54add5bba7dab4502d55eed0e60a678c
achkh17 <achkh17@freeuni.edu.ge>
2019-06-14 15:08:30 +0400

    quiz table created

--- sql/scores.sql
+++ sql/scores.sql
@@ -4,16 +4,15 @@ DROP TABLE IF EXISTS score;
  -- remove table if it already exists and start from scratch
 
 CREATE TABLE IF NOT EXISTS score (
-    quiz_id int not null auto_increment,
+    quiz_id int not null,
     user_id int not null,
     score int default 0,
-    written_time DATETIME,
-    primary key(quiz_id)
+    written_time DATETIME
 );
 
 
 
 
-INSERT INTO score (user_id, score, written_time) VALUES
-    (1, 15, sysdate()),
-    (2, 50, sysdate());
+INSERT INTO score VALUES
+    (1, 1, 15, sysdate()),
+    (3, 2, 50, sysdate());
