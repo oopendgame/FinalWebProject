3315575391897251ba887f37d68501838d4a28e1
achkh17 <achkh17@freeuni.edu.ge>
2019-06-14 21:17:41 +0400

    questuions and answers created. achievements left

---
+++ sql/questions.sql
@@ -0,0 +1,21 @@
+USE finalProject;
+
+DROP TABLE IF EXISTS questions;
+ -- remove table if it already exists and start from scratch
+
+CREATE TABLE IF NOT EXISTS questions (
+    question_id int not null auto_increment,
+    quiz_id int not null,
+    question_type varchar(20) not null default "fillBlank",
+    question varchar(500) not null,
+    
+    primary key(question_id),
+    FOREIGN KEY (quiz_id) REFERENCES quizzes(quiz_id)
+);
+
+
+
+
+INSERT INTO questions(quiz_id, question_type, question) VALUES
+(1, "fillBlank", "dkfnskjgbskgbskgns"),
+(2, "fillBlank", "nsfkjnsgkjs");
