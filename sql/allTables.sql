3697cd8fbac38aa31228354fdb6e9b5bbab33f25
achkh17 <achkh17@freeuni.edu.ge>
2019-06-21 15:55:48 +0400

    finished answer geters and seters for quizzes

--- sql/allTables.sql
+++ sql/allTables.sql
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS answers (
     answer_id int not null auto_increment,
     question_id int not null,
     answer varchar(20) not null,
-    correct_ans int not null default 0,
+    correct_ans boolean not null default 0,
     
     primary key(answer_id),
     FOREIGN KEY (question_id) REFERENCES questions(question_id)
