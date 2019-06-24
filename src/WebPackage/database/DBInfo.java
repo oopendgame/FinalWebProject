b72b4ad90f4d01e6518104c48e7f7ef7fa85b8c3
nchan17 <nchan17@freeuni.edu.ge>
2019-06-24 16:41:54 +0400

    prepare for pull

--- src/WebPackage/database/DBInfo.java
+++ src/WebPackage/database/DBInfo.java
@@ -2,7 +2,7 @@ package WebPackage.database;
 
 public class DBInfo {
 	public static final String MYSQL_USERNAME = "root";
-	public static final String MYSQL_PASSWORD = "Archili_archili";
+	public static final String MYSQL_PASSWORD = "sudopllp";
 	public static final String MYSQL_DATABASE_SERVER = "localhost";
 	public static final String MYSQL_DATABASE_NAME = "finalProject";
 }
