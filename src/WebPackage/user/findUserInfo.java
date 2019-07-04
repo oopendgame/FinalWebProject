311a1334ccd9d500c66f39511b0cf62f404cc898
nchan17 <nchan17@freeuni.edu.ge>
2019-07-05 01:14:38 +0400

    added searching user by id

--- src/WebPackage/user/findUserInfo.java
+++ src/WebPackage/user/findUserInfo.java
@@ -64,6 +64,32 @@ public class findUserInfo {
 		return myUser;
 		
 	}
+	public userInfo getMyUser(int userId) {
+		//userName = "nchan17";
+		ResultSet res = null;
+		userInfo myUser  = null;
+		try {
+			res = stmt.executeQuery("SELECT * from userInfo where user_id = \"" + userId + "\";");
+			if(res.next()) {
+				int myId = res.getInt("user_id");
+				String myUserName = res.getString("user_name");
+				String name = res.getString("first_name");
+				String lastName = res.getString("last_name");
+				String email = res.getString("email");
+				String birthday = res.getString("date_of_birth");
+				String gender = res.getString("gender");
+				String img = res.getString("img");
+				String ranking = res.getString("ranking");
+				myUser = new userInfo(myId, myUserName, name, lastName, img, email, birthday, gender, ranking);
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return myUser;
+		
+	}
 	public void insertUrl(String userName, String setUrl) {
 		//userName = "nchan17";
 		try {
