8033dc540ff9210a1d36bc0872469b1ad03c7825
nchan17 <nchan17@freeuni.edu.ge>
2019-07-04 19:35:56 +0400

    created findMessageInfo.java file

---
+++ src/WebPackage/chat/findMessageInfo.java
@@ -0,0 +1,45 @@
+package WebPackage.chat;
+
+import WebPackage.database.DBInfo;
+
+import java.sql.Connection;
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import WebPackage.database.DBConnection;
+
+public class findMessageInfo {	
+	private Connection con;
+	private Statement stmt;
+	
+	static String account = DBInfo.MYSQL_USERNAME; // replace with your account
+	static String  password = DBInfo.MYSQL_PASSWORD; // replace with your password
+	static String  server = DBInfo.MYSQL_DATABASE_SERVER;
+	static String database = DBInfo.MYSQL_DATABASE_NAME ; // replace with your db
+	
+	public findMessageInfo(){
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection
+						( "jdbc:mysql://" + server, account ,password);
+			stmt = con.createStatement();
+			stmt.executeQuery("USE " + database);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void getMessage() {
+	
+	}
+}
