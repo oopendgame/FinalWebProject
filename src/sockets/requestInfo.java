496813e32a70534b2b98679655c073e72ff91fbc
achkh17 <achkh17@freeuni.edu.ge>
2019-07-04 19:22:18 +0400

    getUserRequests done

--- src/sockets/requestInfo.java
+++ src/sockets/requestInfo.java
@@ -3,16 +3,22 @@ package sockets;
 import java.sql.Date;
 
 public class requestInfo {
+	private int req_id;
 	private int user1;
 	private int user2;
 	private boolean status;
 	private Date sent_date;
 	
-	public requestInfo(int user1_id, int user2_id, boolean stat, Date date) {
+	public requestInfo(int req, int user1_id, int user2_id, boolean stat, Date date) {
+		req_id = req;
 		user1 = user1_id;
 		user2 = user2_id;
 		status = stat;
 		sent_date = date;
+	}
+	
+	public int getRequestId() {
+		return req_id;
 	}
 	
 	public int getSenderID() {
