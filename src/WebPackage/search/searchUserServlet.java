c670908fcbee466497a8bc8925ccfc9d6501ef4c
achkh17 <achkh17@freeuni.edu.ge>
2019-07-06 22:15:01 +0400

    need to remember user_id

--- src/WebPackage/search/searchUserServlet.java
+++ src/WebPackage/search/searchUserServlet.java
@@ -1,11 +1,19 @@
 package WebPackage.search;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import WebPackage.database.DBConnection;
 
 /**
  * Servlet implementation class searchUserServlet
@@ -35,7 +43,24 @@ public class searchUserServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String name = request.getParameter("user_name");
+		
+		int id = 0;
+		try {
+			Connection con = DBConnection.getConnection();
+			Statement stm = con.createStatement();
+			ResultSet res = stm.executeQuery("Select user_id from userInfo where user_name = " + name);
+			if(res.next()) id = Integer.parseInt(res.getString("user_id"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		String st = "UserNotFound.jsp";
+		if(id != 0) st = "";
+		RequestDispatcher rd = request.getRequestDispatcher(st); 
+		rd.forward(request, response); 
 	}
 
 }
