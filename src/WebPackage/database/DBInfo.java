a5975900aa5b1ab3bf7a18db0d09271c938b8186
nchan17 <nchan17@freeuni.edu.ge>
2019-07-07 22:32:05 +0400

    bugs fixed

--- src/WebPackage/database/DBInfo.java
+++ src/WebPackage/database/DBInfo.java
@@ -2,11 +2,8 @@ package WebPackage.database;
 
 public class DBInfo {
 	public static final String MYSQL_USERNAME = "root";
-<<<<<<< HEAD
 	public static final String MYSQL_PASSWORD = "sudopllp";
-=======
-	public static final String MYSQL_PASSWORD = "W)jmz~5%fLW?T86C"; //es datovet xolme komentarshi dzebna mezareba xolme W)jmz~5%fLW?T86C
->>>>>>> 41816d7b827327487a21a807662543d11986603c
+	//public static final String MYSQL_PASSWORD = "W)jmz~5%fLW?T86C"; //es datovet xolme komentarshi dzebna mezareba xolme W)jmz~5%fLW?T86C
 	public static final String MYSQL_DATABASE_SERVER = "localhost";
 	public static final String MYSQL_DATABASE_NAME = "finalProject";
 	public static final String Attribute_Name = "curr";
