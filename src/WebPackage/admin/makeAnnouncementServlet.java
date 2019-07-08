a87f65012d2f5e1fd5e78899e24402f8b61314bd
nchan17 <nchan17@freeuni.edu.ge>
2019-07-08 21:36:09 +0400

    announcement  servlet done

---
+++ src/WebPackage/admin/makeAnnouncementServlet.java
@@ -0,0 +1,50 @@
+package WebPackage.admin;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class makeAnnouncementServlet
+ */
+@WebServlet("/makeAnnouncementServlet")
+public class makeAnnouncementServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public makeAnnouncementServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		announcements info = new announcements();
+		String ann = request.getParameter("announcement");
+		String title = request.getParameter("title");
+		int id = Integer.parseInt(request.getParameter("user"));
+		info.makeAnnouncement(id, title, ann);
+		RequestDispatcher rd = request.getRequestDispatcher("AnnouncementMade.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
