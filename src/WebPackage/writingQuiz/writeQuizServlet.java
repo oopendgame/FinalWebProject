031f459490fda8e4db4d377550250c523863c5d4
achkh17 <achkh17@freeuni.edu.ge>
2019-07-08 23:18:05 +0400

    need sessions for each quiz

--- src/WebPackage/writingQuiz/writeQuizServlet.java
+++ src/WebPackage/writingQuiz/writeQuizServlet.java
@@ -1,11 +1,18 @@
 package WebPackage.writingQuiz;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import WebPackage.quiz.QuestionInfo;
+import WebPackage.quiz.QuizInfo;
+import WebPackage.quiz.findQuestionInfo;
+import WebPackage.quiz.findQuizInfo;
 
 /**
  * Servlet implementation class writeQuizServlet
@@ -36,6 +43,15 @@ public class writeQuizServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
+		int id = Integer.parseInt(request.getParameter("quiz_id"));
+		findQuizInfo quizInfo = new findQuizInfo();
+		QuizInfo quiz = quizInfo.getQuiz(id);
+		
+		findQuestionInfo questInfo = new findQuestionInfo();
+		ArrayList<QuestionInfo> quest = questInfo.getQuizQuestions(id);
+		
+		
 	}
 
 }
