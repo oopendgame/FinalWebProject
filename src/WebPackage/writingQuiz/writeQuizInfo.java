0f52fc841711164f3d7018e8f3229893ff10c0c3
achkh17 <achkh17@freeuni.edu.ge>
2019-07-08 23:59:49 +0400

    writeQuizServlet almost done

---
+++ src/WebPackage/writingQuiz/writeQuizInfo.java
@@ -0,0 +1,31 @@
+package WebPackage.writingQuiz;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import WebPackage.quiz.QuestionInfo;
+import WebPackage.quiz.QuizInfo;
+
+public class writeQuizInfo {
+	private QuizInfo quiz;
+	private ArrayList<QuestionInfo> quest;
+	private Date startTime;
+	
+	public writeQuizInfo(QuizInfo quiz, ArrayList<QuestionInfo> quest, Date startTime) {
+		this.quiz = quiz;
+		this.quest = quest;
+		this.startTime = startTime;
+	}
+	
+	public QuizInfo getQuiz() {
+		return quiz;
+	}
+	
+	public ArrayList<QuestionInfo> getQuestions() {
+		return quest;
+	}
+	
+	public Date getStartTime() {
+		return startTime;
+	}
+}
--- src/WebPackage/writingQuiz/writeQuizServlet.java
+++ src/WebPackage/writingQuiz/writeQuizServlet.java
@@ -1,13 +1,16 @@
 package WebPackage.writingQuiz;
 
 import java.io.IOException;
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import WebPackage.quiz.QuestionInfo;
 import WebPackage.quiz.QuizInfo;
@@ -46,12 +49,17 @@ public class writeQuizServlet extends HttpServlet {
 		
 		int id = Integer.parseInt(request.getParameter("quiz_id"));
 		findQuizInfo quizInfo = new findQuizInfo();
-		QuizInfo quiz = quizInfo.getQuiz(id);
+		QuizInfo curQuiz = quizInfo.getQuiz(id);
 		
 		findQuestionInfo questInfo = new findQuestionInfo();
 		ArrayList<QuestionInfo> quest = questInfo.getQuizQuestions(id);
+		if(curQuiz.getRandom()) Collections.shuffle(quest);
 		
+		Date startTime = new Date((new java.util.Date()).getTime());
+		writeQuizInfo cur = new writeQuizInfo(curQuiz, quest, startTime);
 		
+		HttpSession session = request.getSession();
+		session.setAttribute("writeQuiz", cur);
 	}
 
 }
