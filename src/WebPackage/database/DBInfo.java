11094ba8e7420941fd097d3dbd9da61a9a93e7de
nchan17 <nchan17@freeuni.edu.ge>
2019-07-13 15:54:50 +0400

    minor graphics change

--- src/WebPackage/database/DBInfo.java
+++ src/WebPackage/database/DBInfo.java
@@ -4,12 +4,7 @@ public class DBInfo {
 	public static final String MYSQL_USERNAME = "root";
 	public static final String MYSQL_PASSWORD = "sudopllp";
 //	public static final String MYSQL_PASSWORD = "Archili_archili";
-<<<<<<< HEAD
-	public static final String MYSQL_PASSWORD = "W)jmz~5%fLW?T86C"; //es datovet xolme komentarshi dzebna mezareba W)jmz~5%fLW?T86C
-=======
 //	public static final String MYSQL_PASSWORD = "W)jmz~5%fLW?T86C"; //es datovet xolme komentarshi dzebna mezareba xolme W)jmz~5%fLW?T86C
->>>>>>> 7d112fd8b4c86956ca1c9b1be245332291a07f1c
-
 
 	public static final String MYSQL_DATABASE_SERVER = "localhost";
 	public static final String MYSQL_DATABASE_NAME = "finalProject";
