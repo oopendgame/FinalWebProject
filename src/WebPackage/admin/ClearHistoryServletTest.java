6b8f2036fd237f1f337c7dbf47b337549c3a70c4
nchan17 <nchan17@freeuni.edu.ge>
2019-07-14 18:30:00 +0400

    incorrect jsp name bug fixed

---
+++ src/WebPackage/admin/ClearHistoryServletTest.java
@@ -0,0 +1,41 @@
+package WebPackage.admin;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.jupiter.api.Test;
+
+import WebPackage.user.findUserInfo;
+
+class ClearHistoryServletTest {
+
+	@Test
+	void test() throws ServletException, IOException {
+		HttpServletRequest request = mock(HttpServletRequest.class);       
+	    HttpServletResponse response = mock(HttpServletResponse.class);
+	    when(request.getParameter("quizName")).thenReturn("randQuiz");
+
+	    //adminInfo adInfo = mock(adminInfo.class);
+	    RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+	    findUserInfo info = new findUserInfo();
+	    
+	    //when(adInfo.promoteToAdmin("nchan17")).thenCallRealMethod();
+	    //System.out.println(adInfo.promoteToAdmin("nchan17"));
+	    if(info.isUser("nchan17")) {
+	    	when(request.getRequestDispatcher("success.jsp")).thenReturn(dispatcher);
+	    }else when(request.getRequestDispatcher("incorrectQuizNameClear.jsp")).thenReturn(dispatcher);
+	    ClearHistoryServlet serv = new ClearHistoryServlet();
+	    serv.doPost(request, response);
+		verify(dispatcher).forward(request,response);
+	}
+
+}
