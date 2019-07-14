f12c14071779c0d79779040ca90bcf2cd6f6579b
achkh17 <achkh17@freeuni.edu.ge>
2019-07-14 21:34:36 +0400

    acceptRequestServletTests done

--- src/WebPackage/requests/acceptRequestServletTest.java
+++ src/WebPackage/requests/acceptRequestServletTest.java
@@ -7,6 +7,9 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -16,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.junit.jupiter.api.Test;
 
+import WebPackage.database.DBConnection;
 import WebPackage.database.DBInfo;
 import WebPackage.login.LogInInfo;
 
@@ -27,8 +31,54 @@ public class acceptRequestServletTest {
 	    HttpServletResponse response = mock(HttpServletResponse.class);
 	    RequestDispatcher dispatcher = mock(RequestDispatcher.class);	
 	    
-	    when(request.getParameter("quiz_name")).thenReturn("1");
-	    when(request.getParameter("description")).thenReturn("3");
+	    when(request.getParameter("user1_id")).thenReturn("1");
+	    when(request.getParameter("user2_id")).thenReturn("2");
+	    
+	    Connection con = DBConnection.getConnection();
+		try {
+			Statement nameStm = con.createStatement();
+			nameStm.execute("TRUNCATE friends;");
+			nameStm.execute("INSERT INTO friends(user1_id, user2_id, friends_status, sending_date) VALUES (1, 2, 0, sysdate()), (1, 3, 0, sysdate());");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    findRequestInfo findReq = new findRequestInfo();
+		findReq.addFriend(1, 2);
+	    
+	    StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
+
+        acceptRequestServlet cr = new acceptRequestServlet();
+        
+        when(request.getRequestDispatcher("requestDone.jsp")).thenReturn(dispatcher);
+	    
+	    cr.doGet(request, response);
+		verify(dispatcher).forward(request,response); 
+	}
+	
+	
+	
+	@Test
+	void test2() throws ServletException, IOException {
+		HttpServletRequest request = mock(HttpServletRequest.class);       
+	    HttpServletResponse response = mock(HttpServletResponse.class);
+	    RequestDispatcher dispatcher = mock(RequestDispatcher.class);	
+	    
+	    when(request.getParameter("user1_id")).thenReturn("1");
+	    when(request.getParameter("user2_id")).thenReturn("3");
+	    
+	    Connection con = DBConnection.getConnection();
+		try {
+			Statement nameStm = con.createStatement();
+			nameStm.execute("TRUNCATE friends;");
+			nameStm.execute("INSERT INTO friends(user1_id, user2_id, friends_status, sending_date) VALUES (1, 2, 0, sysdate()), (1, 3, 0, sysdate());");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	    
 	    findRequestInfo findReq = new findRequestInfo();
 		findReq.addFriend(1, 3);
