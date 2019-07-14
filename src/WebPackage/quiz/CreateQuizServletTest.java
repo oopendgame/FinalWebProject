08888b0b5270e6b37e5e4a00bde21f4bc1f3a5c5
achkh17 <achkh17@freeuni.edu.ge>
2019-07-14 17:52:14 +0400

    need context tests

--- src/WebPackage/quiz/CreateQuizServletTest.java
+++ src/WebPackage/quiz/CreateQuizServletTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
 
 import WebPackage.admin.makeAnnouncementServlet;
+import WebPackage.database.DBInfo;
+import WebPackage.login.LogInInfo;
 
 public class CreateQuizServletTest {
 	
@@ -21,16 +24,28 @@ public class CreateQuizServletTest {
 	void test1() throws ServletException, IOException {
 		HttpServletRequest request = mock(HttpServletRequest.class);       
 	    HttpServletResponse response = mock(HttpServletResponse.class);
-	    RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+	    RequestDispatcher dispatcher = mock(RequestDispatcher.class);	
+	    
+	    ServletContext context = mock(ServletContext .class);
+	    when(request.getServletContext()).thenReturn(context);	    
+	    
+	    LogInInfo log = mock(LogInInfo.class);
+	    log.setId(5);
+	    context = request.getServletContext();
+    	context.setAttribute(DBInfo.Attribute_Name, log);
 	    
 	    when(request.getParameter("quiz_name")).thenReturn("quizName");
-	    when(request.getParameter("title")).thenReturn("randTitle");
-	    when(request.getParameter("user")).thenReturn("1");
-	    when(request.getRequestDispatcher("announcementMade.jsp")).thenReturn(dispatcher);
-
-	    makeAnnouncementServlet ann = new makeAnnouncementServlet();
+	    when(request.getParameter("description")).thenReturn("desc");
+	    when(request.getParameter("subject")).thenReturn("subj");
+	    when(request.getParameter("pageNum")).thenReturn("1");
+	    when(request.getParameter("rand")).thenReturn("0");
+	    when(request.getParameter("corr")).thenReturn("0");
 	    
-	    ann.doGet(request, response);
+	    when(request.getRequestDispatcher("QuizDone.jsp")).thenReturn(dispatcher);
+
+	    CreateQuizServlet cr = new CreateQuizServlet();
+	    
+	    cr.doPost(request, response);
 		verify(dispatcher).forward(request,response); 
 	}
 
