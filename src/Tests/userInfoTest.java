66fa15041b92a4fa18d49ecc3ee1c734a87b8709
nchan17 <nchan17@freeuni.edu.ge>
2019-07-14 21:11:15 +0400

    login info test done

--- src/Tests/userInfoTest.java
+++ src/Tests/userInfoTest.java
@@ -9,7 +9,7 @@ import WebPackage.user.userInfo;
 class userInfoTest {
 
 	@Test
-	void test() {
+	void test1() {
 		//public userInfo(int id, String userName, String name, String lastName, String img, String email, String birthday, String gender)
 		int id = 1;
 		String userName = "usName";
@@ -18,6 +18,50 @@ class userInfoTest {
 		String img = "someImg.jpg";
 		String email = "nino@gmail.com";
 		String birthday = "1998-11-09";
+		String gender = "female";
+		userInfo info = new userInfo(id, userName, name, lastName, img, email, birthday, gender);
+		assertEquals(id, info.getId());
+		assertEquals(userName, info.getUserName());
+		assertEquals(name, info.getName());
+		assertEquals(lastName, info.getLastName());
+		assertEquals(img, info.getImg());
+		assertEquals(email, info.getEmail());
+		assertEquals(birthday, info.getBirthday());
+		assertEquals(gender, info.getGender());
+	}
+	
+	@Test
+	void test2() {
+		//public userInfo(int id, String userName, String name, String lastName, String img, String email, String birthday, String gender)
+		int id = 1;
+		String userName = "nchan17";
+		String name = "saxeli";
+		String lastName = "gvari";
+		String img = "surati.jpg";
+		String email = "n@gmail.com";
+		String birthday = "1998-11-09";
+		String gender = "female";
+		userInfo info = new userInfo(id, userName, name, lastName, img, email, birthday, gender);
+		assertEquals(id, info.getId());
+		assertEquals(userName, info.getUserName());
+		assertEquals(name, info.getName());
+		assertEquals(lastName, info.getLastName());
+		assertEquals(img, info.getImg());
+		assertEquals(email, info.getEmail());
+		assertEquals(birthday, info.getBirthday());
+		assertEquals(gender, info.getGender());
+	}
+	
+	@Test
+	void test3() {
+		//public userInfo(int id, String userName, String name, String lastName, String img, String email, String birthday, String gender)
+		int id = 1;
+		String userName = "machkh17";
+		String name = "mariam";
+		String lastName = "chkhaidze";
+		String img = "sulatii.jpg";
+		String email = "machh17@gmail.com";
+		String birthday = "1111-11-11";
 		String gender = "female";
 		userInfo info = new userInfo(id, userName, name, lastName, img, email, birthday, gender);
 		assertEquals(id, info.getId());
