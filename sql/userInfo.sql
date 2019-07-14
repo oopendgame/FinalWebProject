ece3f2fd2c0dc10561f17a4d6cfbf2b57d3e038d
achkh17 <achkh17@freeuni.edu.ge>
2019-07-14 15:49:40 +0400

    inserting basic infos in base

--- sql/userInfo.sql
+++ sql/userInfo.sql
@@ -11,16 +11,15 @@ CREATE TABLE IF NOT EXISTS userInfo (
     email varchar(50) not null,
     date_of_birth date,
     gender varchar(10) not null,
-    img varchar(50),
-    rank varchar(20) default 'begginer',
+    img varchar(2000) default "https://wallpaperplay.com/walls/full/e/3/6/138503.jpg",
     
-    primary key(user_id)
+    primary key(user_id) 
 );
 
 
 
 
-INSERT INTO userInfo (user_name, first_name, last_name, email, date_of_birth, gender, img, ranking) VALUES
-    ('achkh17', 'anamaria', 'chkhaidze', 'achkh17@freeuni.edu.ge', sysdate(), 'female', 'bla.jpg', 'beginner'),
-    ('nchanfd17', 'nindo', 'chantudria', 'nchan17@fdreeuni.edu.ge', sysdate(), 'femdale', 'blura.jpg', 'beginner'),
-    ('nchanss17', 'nino', 'chanturia', 'nchan17@freeuni.edu.ge', sysdate(), 'female', 'blura.jpg', 'beginner');
+INSERT INTO userInfo (user_name, first_name, last_name, email, date_of_birth, gender, img) VALUES
+    ('achkh17', 'anamaria', 'chkhaidze', 'achkh17@freeuni.edu.ge', sysdate(), 'female', 'https://scontent.ftbs4-1.fna.fbcdn.net/v/t1.0-9/51785982_2118103464931868_3119750972641902592_n.jpg?_nc_cat=107&_nc_oc=AQl5w4dHlax4Lwo1ZKTtlUJxYD9tDOqX2DhzHF577f5P4OTqwjhmQXFBGdoVNEs60Tw&_nc_ht=scontent.ftbs4-1.fna&oh=2f6bed7bc491bf7bcc581b2ef5b20a73&oe=5DACCD36'),
+    ('nchan17', 'nino', 'chanturia', 'nchan17@freeuni.edu.ge', sysdate(), 'female', 'https://scontent.ftbs4-1.fna.fbcdn.net/v/t1.0-9/19424401_784494171675882_8786767691181003760_n.jpg?_nc_cat=107&_nc_oc=AQmwxxmh6xwM5jgZmeAt5dgYgKgKVRQId03zDA4d5MzSqxK5fwxfa97IijidsCOuQac&_nc_ht=scontent.ftbs4-1.fna&oh=6f9a3ac188672e613f6f68903129aeeb&oe=5DAEEB25'),
+    ('mchkh17', 'mariam', 'chkhaidze', 'mchkh17@freeuni.edu.ge', sysdate(), 'female', 'https://scontent.ftbs4-1.fna.fbcdn.net/v/t1.0-9/19366221_1510029115708562_394005548772054778_n.jpg?_nc_cat=109&_nc_oc=AQnFER6VlzGE7LMb-0c-VIUWOu0_fzdrvnkDWYWyfTXCpPmT87XnxWlzJdWupv3iNF8&_nc_ht=scontent.ftbs4-1.fna&oh=ea6ba3f13f3fe136196365c8a17597ba&oe=5DC1170D');
